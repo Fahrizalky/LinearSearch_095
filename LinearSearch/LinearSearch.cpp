a04a6dcdfda3b281c251426ed27247a9764d0290
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-04-14 15:23:47 +0700

    menampilkan hasil dari perbandingan dari arr yang bernilai benar

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -45,4 +45,14 @@ void LinearSearch()
 		comparisons = 0;
 		for (i = 0; i < n; i++)//langkah 2,3,dan 4
 		{
-			
+			comparisons++;
+			if (arr[i] == item) //langkah 5A found
+			{
+				cout << "\n" << item << " found at position " << (i + 1) << endl;
+				break;
+			}
+		}
+
+		
+
+
