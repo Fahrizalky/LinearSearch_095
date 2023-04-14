cb3102c336715032ff13eea97ca65c0d1a4aa31f
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-04-14 15:08:42 +0700

    membuat  prosedur void input

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -5,5 +5,15 @@ int arr[20]; // Array to be searched
 int n; // number of elements in the array
 int i; // index off array elements
 
-
-
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of element in the aray : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
+	}
+	
